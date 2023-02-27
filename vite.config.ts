d2121ff34662dd5a169192e48fd2587a8b338aaa
樊宗领 <fanzongling@up366.com>
2023-02-27 17:09:38 +0800

    add: 多页面配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        study: resolve(__dirname, 'study.html')
+      }
+    }
+  },
   plugins: [vue()],
 })
